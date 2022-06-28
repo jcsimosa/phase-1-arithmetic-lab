dc39203905e466ea5ad7d43805cd596194e513ed
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-28 11:17:50 -0500

    commit tree 91b9d09594141c33ee3d1db08deb182a13324186

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31 
+const num2 = 2
+const num3 = 8
+const num4 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10) + 1
+
+let mod = num3 / num4
+
+let max = Math.max(1, 4, 5, 6 ,7,2,4,2,5,7,4,2,20)
